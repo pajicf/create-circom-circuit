d0bce7e1ee3a80cd63f1efed1c50be6166fed2c9
Filip Pajic <fica.pajic@gmail.com>
2023-05-17 17:50:46 +0200

    Compile script added

---
+++ scripts/compile.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+CIRCUIT_NAME=
+if [ "$1" ]; then
+    CIRCUIT_NAME=$1
+else
+  echo "Please provide name of the circuit to compile"
+  exit 1
+fi
+
+circom ./circuits/${CIRCUIT_NAME}.circom --r1cs --wasm --sym -o build
